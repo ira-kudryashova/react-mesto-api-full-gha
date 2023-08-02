bca7cddb8549f89eb4effd25cd8b4673af807acc
Ira Kudryashova <i.trofimova@icloud.com>
2023-08-02 23:00:19 +0300

    prj 15 logger added

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,24 +1,24 @@
 // импортируем нужные модули
-import { transports as _transports, format as _format } from 'winston';
-import { logger, errorLogger as _errorLogger } from 'express-winston';
+const winston = require('winston');
+const expressWinston = require('express-winston');
 
 // создадим логгер запросов
-const requestLogger = logger({
+const requestLogger = expressWinston.logger({
   transports: [ //опуия transports отвечает за то? куда нужно писать лог + это массив? в который можно записать др/транспорты
-    new _transports.File({ filename: 'request.log' }),
+    new winston.transports.File({ filename: 'request.log' }),
   ],
-  format: _format.json(), //опция format отвечает за формат записи логов
+  format: winston.format.json(), //опция format отвечает за формат записи логов
 });
 
 // логгер ошибок
-const errorLogger = _errorLogger({
+const errorLogger = expressWinston.errorLogger({
   transports: [
-    new _transports.File({ filename: 'error.log' }),
+    new winston.transports.File({ filename: 'error.log' }),
   ],
-  format: _format.json(),
+  format: winston.format.json(),
 });
 
-export default {
+module.exports = {
   requestLogger,
   errorLogger,
 };
